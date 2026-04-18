eef93d1dbbca7d10f57b293fd54e9dd630c517fd
DzejPi <dzejjpii@gmail.com>
2026-04-18 21:24:02 +0200

    Finished the long hall section.

--- scripts/game/warp_area.gd
+++ scripts/game/warp_area.gd
@@ -1,6 +1,14 @@
 extends Area3D
 
 
+@onready var removable_wall: Node3D = $RemovableWall
+var is_wall_removed: bool = false
+
+
 func _on_body_entered(body: Node3D) -> void:
 	if body.name == "PlayerScene":
 		body.warp_backwards()
+		
+		if !is_wall_removed:
+			is_wall_removed = true
+			removable_wall.queue_free()
