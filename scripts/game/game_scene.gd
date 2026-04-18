3c75fc1749b4f0d6a5700ffd25d1885e3a652067
DzejPi <dzejjpii@gmail.com>
2026-04-18 20:17:03 +0200

    Made ceilings invisible for easy ediitng, and added script which displays them when the game runs.

--- scripts/game/game_scene.gd
+++ scripts/game/game_scene.gd
@@ -5,9 +5,19 @@ extends Node3D
 @export var child_count_countdown: float = 1
 var current_child_count_countdown: float = child_count_countdown
 
+# Ceilings
+@onready var ceiling_a: Node3D = $Rooms/RoomA/Ceiling
+@onready var ceiling_b: Node3D = $Rooms/RoomB/Ceiling
+@onready var ceiling_c: Node3D = $Rooms/RoomC/Ceiling
+@onready var ceiling_d: Node3D = $Rooms/RoomD/Ceiling
+@onready var ceiling_e: Node3D = $Rooms/RoomE/Ceiling
+@onready var ceiling_f: Node3D = $Rooms/RoomF/Ceiling
+@onready var ceiling_g: Node3D = $Rooms/RoomG/Ceiling
+
+
 
 func _ready() -> void:
-	pass
+	show_all_ceilings()
 
 
 func _process(delta: float) -> void:
@@ -17,3 +27,13 @@ func _process(delta: float) -> void:
 		else:
 			current_child_count_countdown = child_count_countdown
 			print("Game nodes: " + str(get_child_count()))
+
+
+func show_all_ceilings() -> void:
+	ceiling_a.show()
+	ceiling_b.show()
+	ceiling_c.show()
+	ceiling_d.show()
+	ceiling_e.show()
+	ceiling_f.show()
+	ceiling_g.show()
