cd70249c52a058f70c69e41cf18d6019c86d91f6
DzejPi <dzejjpii@gmail.com>
2026-04-19 21:32:13 +0200

    Added sound to the red button.

--- scripts/rooms/objects/object_manager_button.gd
+++ scripts/rooms/objects/object_manager_button.gd
@@ -3,8 +3,10 @@ extends Node3D
 
 @onready var red_button: Node3D = $".."
 @onready var signal_scene: Node3D = $"../SignalScene"
+@onready var audio_stream_player_3d: AudioStreamPlayer3D = $"../AudioStreamPlayer3D"
 
 
 
 func process_object_interaction() -> void:
 	signal_scene.queue_free()
+	audio_stream_player_3d.play()
