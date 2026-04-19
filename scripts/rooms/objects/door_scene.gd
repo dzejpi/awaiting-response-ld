530f0a911fe3a2cde12885cb16f7b9e640f3dc5e
DzejPi <dzejjpii@gmail.com>
2026-04-19 17:25:56 +0200

    Added doors everywhere.

--- scripts/rooms/objects/door_scene.gd
+++ scripts/rooms/objects/door_scene.gd
@@ -2,14 +2,37 @@ extends Node3D
 
 
 var debug: bool = true
-var is_closed: bool = true
+@export var is_force_shut: bool = false
 var can_player_unlock: bool = false
 @onready var door_audio_player: AudioStreamPlayer3D = $DoorAudioPlayer
+@onready var open_trigger: Area3D = $OpenTrigger
+@onready var close_trigger: Area3D = $CloseTrigger
+@onready var door_animation_player: AnimationPlayer = $DoorAnimationPlayer
 
 
 func _ready() -> void:
-	pass
+	if is_force_shut:
+		open_trigger.queue_free()
+		close_trigger.queue_free()
 
 
 func trigger_knocking_sound() -> void:
 	pass
+
+
+func open_doors() -> void:
+	open_trigger.queue_free()
+	door_animation_player.play("open_door")
+
+
+func close_doors() -> void:
+	close_trigger.queue_free()
+	door_animation_player.play("close_doors")
+
+
+func _on_open_trigger_body_entered(body: Node3D) -> void:
+	open_doors()
+
+
+func _on_close_trigger_body_entered(body: Node3D) -> void:
+	close_doors()
