bd07864a36d90bdb80a1cf1623b4909cb53970b8
DzejPi <dzejjpii@gmail.com>
2026-04-19 21:20:36 +0200

    Added sound to the phone.

--- scripts/rooms/objects/object_manager_phone.gd
+++ scripts/rooms/objects/object_manager_phone.gd
@@ -3,7 +3,9 @@ extends Node3D
 
 @onready var computer_full: Node3D = $"."
 @onready var signal_scene: Node3D = $"../SignalScene"
+@onready var audio_stream_player_3d: AudioStreamPlayer3D = $"../AudioStreamPlayer3D"
 
 
 func process_object_interaction() -> void:
 	signal_scene.queue_free()
+	audio_stream_player_3d.queue_free()
